77749dbb221e9de56ec10ccb92c616db3fe18001
Jônatas Fragoso Pedreira <jonatasfrpedreira6@gmail.com>
2026-09-15 10:46:12 -0300

    Adiciona Swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,8 +2,9 @@ import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { InternalTokenGuard } from "../guards/internal-token.guard";
 import { ResolveAuthDto } from "./dto/resolve-auth.dto";
 import { AuthService } from "./auth.service";
+import { ApiTags, ApiBody, ApiOperation, ApiResponse, } from '@nestjs/swagger';
 
-
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
@@ -23,6 +24,32 @@ export class AuthController {
 
     @Post('resolve')
     @UseGuards(InternalTokenGuard)
+    @ApiOperation({
+        summary: 'Resolve uma credencial'
+    })
+    @ApiBody({
+        schema: {
+            type: 'object',
+            properties: {
+                id: {
+                    type: 'number',
+                    example: 99
+                },
+                key: {
+                    type: 'string',
+                    example: 'minha-chave'
+                }
+            }
+        }
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'Autorização concedida'
+    })
+    @ApiResponse({
+        status: 401,
+        description: 'Key inválida ou ID não encontrado'
+    })
     async resolve(@Body() body: ResolveAuthDto,){
         return this.authService.resolve(
             body.id,
