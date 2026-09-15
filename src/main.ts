b293b01a661b18fbab89dbd766cdc60399656c7f
Jônatas Fragoso Pedreira <jonatasfrpedreira6@gmail.com>
2026-09-15 11:40:58 -0300

    Atualiza Swagger com Token

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
     .setTitle('ResolvePS API')
     .setDescription('Documentação da API ResolvePS')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   
   const document = SwaggerModule.createDocument(app, config);
